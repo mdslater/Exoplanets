634170b77917c9666914e5eda810f8988a0a0a11
mdslater <mdslater721@gmail.com>
2016-06-14 17:52:25 +0200

    Update revised_mask.py

--- revised_mask.py
+++ revised_mask.py
@@ -146,9 +146,6 @@
   numbers1=[0,-1,-2,-3,-4,-5,-6,-7,1,2,3,4,5,6,7]
   numbers2=[0,-1,-2,-3,-4,-5,-6,-7,1,2,3,4,5,6,7]
 
-
-
-
   for i in range(len(radius)):		
   	if radius[i] > 0.:	
   		for n1 in numbers1:
